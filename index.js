acbeb8d7645be3d2ce775123a241e9626ad564f9
AshleyEvonne <ashleyevonne345@gmail.com>
2024-06-10 11:58:17 -0400

    updated Dom Lab

--- index.js
+++ index.js
@@ -21,11 +21,22 @@ topMenuEl.style.background = "var(--top-menu-bg)";
 
 topMenuEl.classList.add("flex-around");
 
-const menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
+let menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
   ];
 
   menuLinks.forEach((link) => {
@@ -38,4 +49,50 @@ const menuLinks = [
     // Append the new element to the topmenuEl element
     topMenuEl.appendChild(aElement);
   });
+//PART 2
+const submenuEl = document.querySelector("#sub-menu");
 
+submenuEl.style.height = "100%" //Adding height to the sub menu
+
+submenuEl.style.background = "var(--sub-menu-bg)"; // adding a bckground color to sub menu
+
+submenuEl.classList.add ("flex-around"); // 
+
+submenuEl.style.position = "absolute";
+
+submenuEl.style.top ="0";
+
+const topMenuLinks = topMenuEl.querySelectorAll("a");
+console.log(topMenuLinks);
+
+topMenuEl.addEventListener("click", (event) => {
+    event.preventDefault();
+    //console.log(event.target.tagName);
+    if (event.target.tagName !== "A") return;
+});
+
+// topMenuLinks.forEach(link =>{
+//         if(link === event.target){
+//             link.classList.toggle("active");
+//         } else {
+//             link.classList.remove("active");
+//         }
+//     });
+const linkText = event.target.textContext.toLowercase();
+const linkObject = menuLinks.find((link) => link.text === linkText);
+
+if (event.target.classList.contains("active")) {
+    event.target.classList.remove("active");
+    submenuEl.style.top = "0";
+} else {
+    document.querySelectorAll("#top-menu a").forEach((a) => a.classList.remove("active"));
+    event.target.classList.add("active");
+    if (linkObject.subLinks) {
+      submenuEl.style.top = "100%";
+      buildSubmenu (linkObject.subLinks);
+    } else {
+      submenuEl.style.top = "0";
+    }
+
+}
+// });
